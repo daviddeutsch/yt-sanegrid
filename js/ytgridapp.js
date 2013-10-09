8b3dff07433fc814c12e5e1de2d0c6392a861ea0
David Deutsch <skore@valanx.org>
2013-10-10 00:22:37 +0200

    re #82, re #85 Responsive footer

--- js/ytgridapp.js
+++ js/ytgridapp.js
@@ -562,6 +562,30 @@ ytsubgridApp.controller( 'UpdatesModalInstanceCtrl',
 		}]
 );
 
+ytsubgridApp.controller( 'SettingsTabsCtrl',
+	[ '$rootScope', '$scope',
+		function ($rootScope, $scope) {
+			$scope.tabs = [];
+
+			$scope.navType = 'pills';
+		}]
+);
+
+var TabsDemoCtrl = function ($scope) {
+	$scope.tabs = [
+		{ title:"Dynamic Title 1", content:"Dynamic content 1" },
+		{ title:"Dynamic Title 2", content:"Dynamic content 2", disabled: true }
+	];
+
+	$scope.alertMe = function() {
+		setTimeout(function() {
+			alert("You've selected the alert tab!");
+		});
+	};
+
+	$scope.navType = 'pills';
+};
+
 ytsubgridApp.factory( 'appLoading',
 	['$rootScope',
 	function ( $rootScope ) {
