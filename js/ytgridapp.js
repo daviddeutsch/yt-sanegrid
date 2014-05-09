4e2fc1401781f1a7e9af8e7c60293fada1b84d81
David Deutsch <skore@valanx.org>
2014-05-09 09:57:37 +0200

    checking whether we get ANYTHING here

--- js/ytgridapp.js
+++ js/ytgridapp.js
@@ -746,6 +746,8 @@ ytsubgridApp.service( 'googleAPI',
 				}, function( result ) {
 					self.result = result;
 
+					console.log(result);
+
 					if ( result === null ) {
 						self.gapi.client.load('youtube', 'v3', function(response) {
 							deferred.resolve();
